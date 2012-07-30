d7bcaa123a550e316622851f646418be3d08124e
Lukas Marek <lukas.marek@gmail.com>
2012-07-30 16:54:15 +0200

    Secured controllers

--- grails-app/controllers/cz/urbo/user/RoleController.groovy
+++ grails-app/controllers/cz/urbo/user/RoleController.groovy
@@ -2,6 +2,7 @@ package cz.urbo.user
 
 import grails.plugins.springsecurity.Secured
 
+@Secured(['ROLE_ADMIN'])
 class RoleController {
     static def scaffold = Role
 }
--- grails-app/controllers/cz/urbo/user/UserRoleController.groovy
+++ grails-app/controllers/cz/urbo/user/UserRoleController.groovy
@@ -2,6 +2,7 @@ package cz.urbo.user
 
 import grails.plugins.springsecurity.Secured
 
+@Secured(['ROLE_ADMIN'])
 class UserRoleController {
     static def scaffold = UserRole
 }
