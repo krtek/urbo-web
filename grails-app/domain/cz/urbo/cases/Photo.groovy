081c98bc0446dfd4baab26d5653cf45692fb4641
Lukas Marek <lukas.marek@gmail.com>
2012-07-31 17:27:35 +0200

    Photo size

--- grails-app/domain/cz/urbo/cases/Photo.groovy
+++ grails-app/domain/cz/urbo/cases/Photo.groovy
@@ -4,7 +4,7 @@ class Photo {
 
     static constraints = {
         // Limit upload file size to 1MB
-        data maxSize: 1024 * 1024 * 1 // TODO michal bernhard: fluent interface like http://wicket.apache.org/apidocs/1.4/org/apache/wicket/util/lang/Bytes.html
+        data maxSize: 1024 * 1024 * 4 // TODO michal bernhard: fluent interface like http://wicket.apache.org/apidocs/1.4/org/apache/wicket/util/lang/Bytes.html
     }
 
     byte[] data
