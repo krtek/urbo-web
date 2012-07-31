ad60ae98a87a5e09811b757c64fbd0ca2d018d8d
Lukas Marek <lukas.marek@gmail.com>
2012-07-31 17:35:14 +0200

    Removed password

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -102,7 +102,7 @@ grails {
         host = "smtp.gmail.com"
         port = 465
         username = "info@urbo.cz"
-        password = "***"
+        password = "xxx"
         props = ["mail.smtp.auth":"true",
                 "mail.smtp.socketFactory.port":"465",
                 "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
