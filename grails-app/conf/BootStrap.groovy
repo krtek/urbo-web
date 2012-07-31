a24c5406db1ada8928b212af17e64d1690350c4f
Lukas Marek <lukas.marek@gmail.com>
2012-07-31 17:19:02 +0200

    Create-drop even the production DB - there are no vital data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -17,15 +17,18 @@ class BootStrap {
             println "Development environment"
 
             FeedbackTestUtils.createTestingFeedbacks()*.save()
-
-            def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-            def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
-
-            def testUser = new User(username: 'me', enabled: true, password: 'password')
-            testUser.save(flush: true)
-
-            UserRole.create testUser, adminRole, true
         }
+
+        //Remove this ASAP :-)
+
+        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+        def testUser = new User(username: 'me', enabled: true, password: 'password')
+        testUser.save(flush: true)
+
+        UserRole.create testUser, adminRole, true
+
 
     }
     def destroy = {
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -26,7 +26,7 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
+            dbCreate = "create-drop"
             driverClassName = "org.postgresql.Driver"
             dialect = org.hibernate.dialect.PostgreSQLDialect
 
