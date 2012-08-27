248d139fc74ac595818028a37d4dbc6a4f85d0d3
Michal Bernhard <michal@bernhard.cz>
2012-08-27 14:40:07 +0200

    tests for GoogleGeoCodeApiService

--- test/integration/api/GoogleGeoCodeApiServiceTest.groovy
+++ test/integration/api/GoogleGeoCodeApiServiceTest.groovy
@@ -1,11 +1,34 @@
 package api
 
-/**
- * Created with IntelliJ IDEA.
- * User: michal
- * Date: 8/24/12
- * Time: 4:40 PM
- * To change this template use File | Settings | File Templates.
- */
-class GoogleGeoCodeApiServiceTest extends GroovyTestCase { {
+import cz.urbo.cases.Location
+import grails.test.mixin.TestFor
+import org.junit.Test
+import web.GoogleGeoCodeApiService
+
+@TestFor(GoogleGeoCodeApiService)
+class GoogleGeoCodeApiServiceTest extends GroovyTestCase {
+
+    @Test
+    public void townShouldBeResolved() {
+
+        /* this is not needed as service variable is available thanks to @TestFor ast transformation */
+        // def service = new GoogleGeoCodeApiService()
+
+        def address = service.transformLocationToAddress(new Location(latitude: 50.076, longitude: 14.408))
+        assertEquals("Praha 5", address.town)
+
+    }
+
+    @Test
+    public void streetNameShouldBeResolved() {
+        def address = service.transformLocationToAddress(new Location(latitude: 50.076, longitude: 14.408))
+        assertEquals("Kořenského", address.streetName)
+    }
+
+    @Test
+    public void streetNumberShouldBeResolved() {
+        def address = service.transformLocationToAddress(new Location(latitude: 50.076, longitude: 14.408))
+        assertEquals("3", address.streetNumber)
+    }
+
 }
